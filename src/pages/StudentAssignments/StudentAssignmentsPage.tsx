1098f450010b7f66ee975828f7db2f4d183ad0f4
Ruslan Voshchylo <ruslan.voshchylo2@gmail.com>
2024-11-11 12:40:12 +0200

    add empty states for assignments student

--- src/pages/StudentAssignments/StudentAssignmentsPage.tsx
+++ src/pages/StudentAssignments/StudentAssignmentsPage.tsx
@@ -13,7 +13,7 @@ export const StudentAssignmentsPage = () => {
   const { user } = useContext(UserContext);
   const [openAssignment, setOpenAssignment] = useState<IAssignment[]>([]);
   const [closedAssignment, setClosedAssignment] = useState<IAssignment[]>([]);
-  
+
   const [selectedTab, setSelectedTab] = useState("open");
   const { data: assignments, refetch, isRefetching } = useGetStudentAssignments(user?.email ?? "");
 
@@ -43,52 +43,56 @@ export const StudentAssignmentsPage = () => {
         <div className="w-full">
           <div className='flex gap-2'>
             <div className={`flex-1 flex justify-center border-b transition-all pb-2 ${selectedTab === "open" ? 'font-semibold border-[#CC1316] border-b' : 'border-transparent'}`}
-            onClick={() => setSelectedTab('open')}
+              onClick={() => setSelectedTab('open')}
             >
               Open
             </div>
             <div className={`flex-1 flex justify-center border-b transition-all pb-2 ${selectedTab === "closed" ? 'font-semibold border-[#CC1316] border-b' : 'border-transparent'}`}
-            onClick={() => setSelectedTab('closed')}
+              onClick={() => setSelectedTab('closed')}
             >
               Closed
             </div>
           </div>
-          {selectedTab === 'open' && (
+          {selectedTab === 'open' && !isRefetching &&
             <ul className="py-[20px] flex flex-col gap-[8px]">
-            {openAssignment?.map((assignment) => (
-              <Link
-                key={assignment.id}
-                to={`/student-assignments/${assignment.id}`}
-                className="block"
-              >
-                <li
-                  className="w-full block p-[10px] rounded-[14px] bg-gray-200"
+              {openAssignment.length ? openAssignment?.map((assignment) => (
+                <Link
+                  key={assignment.id}
+                  to={`/student-assignments/${assignment.id}`}
+                  className="block"
                 >
-                  <p className="font-semibold">Title: <span className="font-light">{assignment.title}</span></p>
-                  <p className="font-semibold">Topic: <span className="font-light">{assignment.topic}</span></p>
-                </li>
-              </Link>
-            ))}
-          </ul>
-          )}
-          {selectedTab === 'closed' && (
+                  <li
+                    className="w-full block p-[10px] rounded-[14px] bg-gray-200"
+                  >
+                    <p className="font-semibold">Title: <span className="font-light">{assignment.title}</span></p>
+                    <p className="font-semibold">Topic: <span className="font-light">{assignment.topic}</span></p>
+                  </li>
+                </Link>
+              )) :
+                <div className='self-center flex-1 flex justify-center mt-5'>No open assignments</div>
+              }
+            </ul>
+          }
+          {selectedTab === 'closed' && !isRefetching &&
             <ul className="py-[20px] flex flex-col gap-[8px]">
-            {closedAssignment?.map((assignment) => (
-              <Link
-                key={assignment.id}
-                to={`/student-assignments/${assignment.id}`}
-                className="block"
-              >
-                <li
-                  className="w-full block p-[10px] rounded-[14px] bg-gray-200"
+              {closedAssignment.length ? closedAssignment?.map((assignment) => (
+                <Link
+                  key={assignment.id}
+                  to={`/student-assignments/${assignment.id}`}
+                  className="block"
                 >
-                  <p className="font-semibold">Title: <span className="font-light">{assignment.title}</span></p>
-                  <p className="font-semibold">Topic: <span className="font-light">{assignment.topic}</span></p>
-                </li>
-              </Link>
-            ))}
-          </ul>
-          )}
+                  <li
+                    className="w-full block p-[10px] rounded-[14px] bg-gray-200"
+                  >
+                    <p className="font-semibold">Title: <span className="font-light">{assignment.title}</span></p>
+                    <p className="font-semibold">Topic: <span className="font-light">{assignment.topic}</span></p>
+                  </li>
+                </Link>
+              )) :
+                <div className='self-center flex-1 flex justify-center mt-5'>No closed assignments</div>
+              }
+            </ul>
+          }
         </div>
       </div>
     </div>
