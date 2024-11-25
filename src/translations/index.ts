709274852f789ee05c1597a949e2fe82c1f06602
YuriiBogonos <ubogonos2003@gmail.com>
2024-11-25 13:20:16 +0200

    added spanish translations

--- src/translations/index.ts
+++ src/translations/index.ts
@@ -1,7 +1,7 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
-import { EN, PT } from "./locales";
+import { EN, PT, SP } from "./locales";
 
 const resources = {
   en: {
@@ -10,6 +10,9 @@ const resources = {
   pt: {
     translation: PT,
   },
+  sp: {
+    translation: SP,
+  }
 };
 
 i18n.use(initReactI18next).init({
--- src/translations/locales/index.ts
+++ src/translations/locales/index.ts
@@ -1,11 +1,18 @@
 import AUTH_PAGES_EN from "./auth-pages/auth-pages.en.json";
 import AUTH_PAGES_PT from "./auth-pages/auth-pages.pt.json";
+import AUTH_PAGES_SP from "./auth-pages/auth-pages.sp.json";
+
 import STUDENT_PAGES_EN from "./student-pages/student-pages.en.json";
 import STUDENT_PAGES_PT from "./student-pages/student-pages.pt.json";
+import STUDENT_PAGES_SP from "./student-pages/student-pages.sp.json";
+
 import CONVERSATION_PAGE_EN from "./conversation-page/conversation-page.en.json";
 import CONVERSATION_PAGE_PT from "./conversation-page/conversation-page.pt.json";
+import CONVERSATION_PAGE_SP from "./conversation-page/conversation-page.sp.json";
+
 import TEACHER_PAGES_EN from "./teacher-pages/teacher-pages.en.json";
 import TEACHER_PAGES_PT from "./teacher-pages/teacher-pages.pt.json";
+import TEACHER_PAGES_SP from "./teacher-pages/teacher-pages.sp.json";
 
 export const EN = {
   ...AUTH_PAGES_EN,
@@ -19,4 +26,11 @@ export const PT = {
   ...STUDENT_PAGES_PT,
   ...CONVERSATION_PAGE_PT,
   ...TEACHER_PAGES_PT,
+};
+
+export const SP = {
+  ...AUTH_PAGES_SP,
+  ...STUDENT_PAGES_SP,
+  ...CONVERSATION_PAGE_SP,
+  ...TEACHER_PAGES_SP,
 };
