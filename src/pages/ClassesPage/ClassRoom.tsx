ded703dd6da462f31d5bf0e1af8173f4c380c2e8
YuriiBogonos <ubogonos2003@gmail.com>
2024-11-25 16:02:56 +0200

    hot fix

--- src/pages/ClassesPage/ClassRoom.tsx
+++ src/pages/ClassesPage/ClassRoom.tsx
@@ -36,7 +36,7 @@ const ClassesPage = () => {
         {(isPending || isRefetching) && <Loader />}
         <div className="flex items-center justify-between mb-4">
           <h1 className="text-2xl font-semibold text-[#524344]">{t("teacherPages.classes.headerTitle")}</h1>
-          <button className="bg-main text-white w-8 h-8 rounded-full flex items-center justify-center mr-[70px]">
+          <button className="bg-main text-white w-8 h-8 rounded-full flex items-center justify-center mr-[90px]">
             <img className="w-[70%] h-[70%]" src={plus} onClick={openModal} />
           </button>
         </div>
