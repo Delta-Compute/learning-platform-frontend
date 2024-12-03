9c8cb2caaf3560bedbf41da0d966695c5bb13483
YuriiBogonos <ubogonos2003@gmail.com>
2024-12-03 11:14:22 +0200

    removing ai authorization from initial page

--- src/pages/Initial/InitialPage.tsx
+++ src/pages/Initial/InitialPage.tsx
@@ -1,66 +1,15 @@
-import { ChangeEvent, useContext, useState } from "react";
+import { useContext } from "react";
 
 import { useTranslation } from "react-i18next";
 
-import { UsersApiService } from "../../services";
+import { Link } from "react-router-dom";
 
-import { Link, useNavigate } from "react-router-dom";
-
-import { Button, Input, Modal } from "../../components";
-
-import SchoolNamesContext, {School} from "../../context/SchoolNamesContext";
+import SchoolNamesContext, { School } from "../../context/SchoolNamesContext";
 import { SCHOOL_LOGOS } from "./data/school-logos";
-
-import { cn } from "../../utils";
-
-import { UserAuthType } from "../../types";
 
 export const InitialPage = () => {
   const { t } = useTranslation();
   const { currentSchoolName } = useContext(SchoolNamesContext);
-  const navigate = useNavigate();
-
-  const [isAiModalOpen, setIsAiModalOpen] = useState(false);
-  const [emailForAi, setEmailForAi] = useState("");
-  const [emailError, setEmailError] = useState("");
-
-  const handleOpenAi = () => {
-    setIsAiModalOpen(true);
-  };
-
-  const validateEmail = (email: string) => {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
-  };
-
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const email = e.target.value;
-    setEmailForAi(email);
-
-    if (!validateEmail(email)) {
-      setEmailError("Please enter a valid email address");
-    } else {
-      setEmailError("");
-    }
-  };
-
-  const handleNavigate = async () => {
-    if (emailError) {
-      return;
-    }
-
-    try {
-      const fetchedUsers = await UsersApiService.findUserByEmail(emailForAi, currentSchoolName, UserAuthType.AI);
-
-      if (fetchedUsers.length > 0) {
-        navigate(`/${currentSchoolName}/auth-with-ai`);
-      } else {
-        navigate(`/${currentSchoolName}/introducing-with-ai`);
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  }
 
   return (
     <div
@@ -101,24 +50,15 @@ export const InitialPage = () => {
         </p>
 
         <div className="flex flex-col gap-2 items-center justify-center w-full px-3">
-          <Button
-            onClick={handleOpenAi}
+          <Link
+            to={`/${currentSchoolName}/sign-up`}
             className={`
               ${currentSchoolName === School.Educare ? "bg-white text-main" : "bg-main text-white"}
-              p-3 w-[100%] border-[1px] flex items-center justify-center gap-2
+              w-[360px] py-4 rounded-full text-center
             `}
           >
-            <svg fill="currentColor" className="w-[30px] h-[30px]" viewBox="0 0 24 24" role="img" xmlns="http://www.w3.org/2000/svg">
-              <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-              <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
-              <g id="SVGRepo_iconCarrier"><title>OpenAI icon</title>
-                <path
-                  d="M22.2819 9.8211a5.9847 5.9847 0 0 0-.5157-4.9108 6.0462 6.0462 0 0 0-6.5098-2.9A6.0651 6.0651 0 0 0 4.9807 4.1818a5.9847 5.9847 0 0 0-3.9977 2.9 6.0462 6.0462 0 0 0 .7427 7.0966 5.98 5.98 0 0 0 .511 4.9107 6.051 6.051 0 0 0 6.5146 2.9001A5.9847 5.9847 0 0 0 13.2599 24a6.0557 6.0557 0 0 0 5.7718-4.2058 5.9894 5.9894 0 0 0 3.9977-2.9001 6.0557 6.0557 0 0 0-.7475-7.0729zm-9.022 12.6081a4.4755 4.4755 0 0 1-2.8764-1.0408l.1419-.0804 4.7783-2.7582a.7948.7948 0 0 0 .3927-.6813v-6.7369l2.02 1.1686a.071.071 0 0 1 .038.052v5.5826a4.504 4.504 0 0 1-4.4945 4.4944zm-9.6607-4.1254a4.4708 4.4708 0 0 1-.5346-3.0137l.142.0852 4.783 2.7582a.7712.7712 0 0 0 .7806 0l5.8428-3.3685v2.3324a.0804.0804 0 0 1-.0332.0615L9.74 19.9502a4.4992 4.4992 0 0 1-6.1408-1.6464zM2.3408 7.8956a4.485 4.485 0 0 1 2.3655-1.9728V11.6a.7664.7664 0 0 0 .3879.6765l5.8144 3.3543-2.0201 1.1685a.0757.0757 0 0 1-.071 0l-4.8303-2.7865A4.504 4.504 0 0 1 2.3408 7.872zm16.5963 3.8558L13.1038 8.364 15.1192 7.2a.0757.0757 0 0 1 .071 0l4.8303 2.7913a4.4944 4.4944 0 0 1-.6765 8.1042v-5.6772a.79.79 0 0 0-.407-.667zm2.0107-3.0231l-.142-.0852-4.7735-2.7818a.7759.7759 0 0 0-.7854 0L9.409 9.2297V6.8974a.0662.0662 0 0 1 .0284-.0615l4.8303-2.7866a4.4992 4.4992 0 0 1 6.6802 4.66zM8.3065 12.863l-2.02-1.1638a.0804.0804 0 0 1-.038-.0567V6.0742a4.4992 4.4992 0 0 1 7.3757-3.4537l-.142.0805L8.704 5.459a.7948.7948 0 0 0-.3927.6813zm1.0976-2.3654l2.602-1.4998 2.6069 1.4998v2.9994l-2.5974 1.4997-2.6067-1.4997Z"></path>
-              </g>
-            </svg>
-
-            <span>{t("authPages.initial.identifyButton")}</span>
-          </Button>
+            {t("authPages.initial.signUpButton")}
+          </Link>
 
           <Link
             className={`
@@ -131,30 +71,6 @@ export const InitialPage = () => {
           </Link>
         </div>
       </div>
-
-      <Modal isOpen={isAiModalOpen} onClose={() => setIsAiModalOpen(false)}>
-        <div className="flex flex-col items-center gap-1">
-          <h2 className="text-[24px] font-semibold text-center text-[#001434]">
-            {t("authPages.signIn.aiAuthModalTitle")}
-          </h2>
-          <Input
-            type="email"
-            value={emailForAi}
-            onChange={(e) => handleChange(e)}
-            placeholder={t("authPages.signIn.aiAuthEmailPlaceholder")}
-            className={cn("w-full mt-4", emailError && "border-red-500")}
-            required
-          />
-          {emailError && <p className="text-red-500 mt-2">{emailError}</p>}
-
-          <Button
-            className="mt-4 bg-main text-white w-full"
-            onClick={() => handleNavigate()}
-          >
-            {t("authPages.initial.aiModal.submitButton")}
-          </Button>
-        </div>
-      </Modal>
     </div>
   );
 };
