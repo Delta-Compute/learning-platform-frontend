0d5202645c2d98036f5517398fc81f1080999f94
YuriiBogonos <ubogonos2003@gmail.com>
2024-12-20 14:21:51 +0200

    hot fix

--- src/pages/ClassRooms/ClassRoomsPage.tsx
+++ src/pages/ClassRooms/ClassRoomsPage.tsx
@@ -7,8 +7,6 @@ import SchoolNamesContext from "../../context/SchoolNamesContext";
 import { useGetClassesTeacherId } from '../../hooks/api/classes';
 import UserContext from '../../context/UserContext';
 
-import { UserRole } from "../../types";
-
 import { Loader, CreateClassModal, BottomNavigation } from "../../components";
 
 import { useNavigate, Link } from "react-router-dom";
